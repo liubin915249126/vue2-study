42072fe492e7180b759f952fcfcf8de4cfdec541
liubin <915249126@qq.com>
2017-10-15 14:47:34 +0800

    父子组件测试

---
+++ Script/src/component-b.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="message" id="componentB">{{ msg }}</div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    // let Vue = new Vue;
+    export default Vue.component('my-component', {
+            template: '#componentB ',
+            data(){
+                return {
+                      msg: 'component-B'
+                    }
+            }
+    })
+</script>
+
+<style>
+</style>
---
+++ Script/src/component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+   <div class="message">父子组件测试</div>
+   <component-A></component-A>
+   <component-B></component-B>
+  </div>  
+</template>
+
+<script>
+    import componentA from './component-a.vue';
+    import componentB from './component-b.vue'
+    export default {
+    data () {
+        return {
+       
+        }
+     },
+     components:{
+         "component-A":componentA,
+         "component-B":componentB
+     }
+    }
+</script>
+
+<style>
+   
+</style>
