5d9a47483cfb1f4cdbc1e648794d5b3fa6407e0c
liubin <915249126@qq.com>
2017-10-21 15:09:36 +0800

    修改我的文章筛选

--- Script/src/myarticle/articleList.vue
+++ Script/src/myarticle/articleList.vue
@@ -5,11 +5,84 @@
         <el-breadcrumb-item>文章列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-row class="list-content">
-       <el-col :span="5">
-         
+       <el-col :span="5" class="content-left">
+         <div class="btnwrap">
+            <el-button type="primary">发布文章</el-button>
+         </div>
+         <div class="filterwrap">
+           <span>筛选</span>
+           <div>
+           <el-date-picker
+            v-model="daterange"
+            size="small"
+            type="daterange"
+            placeholder="选择日期范围">
+           </el-date-picker>
+           </div>
+           <div>
+             <el-input
+                placeholder="请输入关键字"
+                icon="search"
+                size="small"
+                v-model="keysearch"
+                :on-icon-click="keysearchFun">
+             </el-input>
+           </div>
+           <el-row>
+             <el-col :span="5">状态:</el-col>
+             <el-col :span="19">
+                <el-select v-model="stausvalue" clearable placeholder="请选择" size="small">
+                  <el-option
+                    v-for="item in statusoptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+             </el-col>
+           </el-row>
+           <el-row>
+             <el-col :span="5">标签:</el-col>
+             <el-col :span="19"><el-input v-model="labelvalue" placeholder="请输入内容" size="small"></el-input></el-col>
+           </el-row>
+           <el-row>
+             <el-col :span="5">类型:</el-col>
+             <el-col :span="19">
+                <el-select v-model="typevalue" clearable placeholder="请选择" size="small">
+                  <el-option
+                    v-for="item in typeoptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+             </el-col>
+           </el-row>
+         </div>
        </el-col>
-       <el-col :span="19">
-         
+       <el-col :span="19" class="content-right">
+          <div class="right-inner">
+             <div class="title">我的文章</div>
+                  <el-table
+                    :data="tableData"
+                    stripe
+                    style="width: 100%">
+                    <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+                    </el-table-column>
+                    <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="180">
+                    </el-table-column>
+                    <el-table-column
+                    prop="address"
+                    label="地址">
+                    </el-table-column>
+                </el-table>
+          </div>
        </el-col>
     </el-row>
   </div>
@@ -20,8 +93,23 @@
   import './style/index.less'
   export default {
       data(){
-          return{}
-      }
+          return{
+              daterange:'',
+              keysearch:'',
+              stausvalue:'',
+              statusoptions:[],
+              labelvalue:'',
+              typevalue:'',
+              typeoptions:[],
+              coldata:[]
+              table:[]
+          }
+      },
+      methods:{
+          keysearchFun:function(){
+
+          }
+      } 
   } 
 </script>
 
