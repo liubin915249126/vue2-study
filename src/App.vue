c5318e4c7ccd3ac946e21f6aad390068d4ed402b
len.liu <915249126@qq.com>
2022-05-25 12:28:17 +0800

    feat: add template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view></router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createRouter,createWebHashHistory } from 'vue-router'
+import App from './App.vue'
+
+import HelloWorld from './components/HelloWorld.vue'
+
+const routes = [
+    { path: '/', component: HelloWorld },
+  ]
+
+const router = createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+  })
+
+createApp(App)
+.use(router)
+.mount('#app')
